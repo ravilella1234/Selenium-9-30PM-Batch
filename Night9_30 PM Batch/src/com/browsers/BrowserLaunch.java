81f5473856b34723335b74f38c85180ce5d90eea
masters.selenium <masters.selenium@gmail.com>
2019-04-18 22:32:24 +0530

    Browser Launching code

--- Night9_30 PM Batch/src/com/browsers/BrowserLaunch.java
+++ Night9_30 PM Batch/src/com/browsers/BrowserLaunch.java
@@ -1,20 +1,38 @@
 package com.browsers;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class BrowserLaunch 
 {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\Drivers\\chromedriver.exe");
-		ChromeDriver driver1=new ChromeDriver();
-		driver1.get("https://www.amazon.in");
+		WebDriver driver;
 		
-		//webdriver.gecko.driver
-		//webdriver.ie.driver
-		//webdriver.edge.driver
-
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.amazon.in");
+		
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\Drivers\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//drivers//geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.get("https://www.bestbuy.com");
+		
+		//System.setProperty("webdriver.ie.driver", "C:\\Users\\DELL\\Desktop\\Drivers\\IEDriverServer.exe");
+		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"//drivers//IEDriverServer.exe");
+		driver=new InternetExplorerDriver();
+		driver.get("https://www.snapdeal.com");
+		
+		//System.setProperty("webdriver.edge.driver","C:\\Users\\DELL\\Desktop\\Drivers\\MicrosoftWebDriver.exe" );
+		System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe" );
+		driver=new EdgeDriver();
+		driver.get("https://www.ebay.com");
+		
 	}
 
 }
---
+++ Night9_30 PM Batch/src/com/browsers/TC_002.java
@@ -0,0 +1,14 @@
+package com.browsers;
+
+public class TC_002 extends BaseTest
+{
+
+	public static void main(String[] args) 
+	{
+		launch("ie");
+		
+		navigate("https://www.bestbuy.com");
+
+	}
+
+}
