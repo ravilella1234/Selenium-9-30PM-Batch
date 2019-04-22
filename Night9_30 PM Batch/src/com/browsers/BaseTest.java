a21330a29ac5b6208eeb45729ea138b902255e2f
ravilella <ravilella.1981@gmail.com>
2019-04-22 22:25:21 +0530

    Browser Commands

--- Night9_30 PM Batch/src/com/browsers/BaseTest.java
+++ Night9_30 PM Batch/src/com/browsers/BaseTest.java
@@ -5,9 +5,12 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerDriverService;
 
 public class BaseTest 
 {
@@ -25,16 +28,19 @@ public class BaseTest
 	public static void launch(String browserKey)
 	{
 		if(p.getProperty(browserKey).equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
+			System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//chromedriver.exe");
 			driver=new ChromeDriver();
 		}else if(p.getProperty(browserKey).equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//drivers//geckodriver.exe");
 			driver=new FirefoxDriver();
 		}else if(p.getProperty(browserKey).equals("ie")) {
-			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"//drivers//IEDriverServer.exe");
+			//System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"//drivers//IEDriverServer.exe");
+			System.setProperty(InternetExplorerDriverService.IE_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//IEDriverServer.exe");
 			driver=new InternetExplorerDriver();
 		}else if(p.getProperty(browserKey).equals("edge")) {
-			System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe" );
+			//System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe" );
+			System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY,System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe" );
 			driver=new EdgeDriver();
 		}
 		
@@ -42,7 +48,8 @@ public class BaseTest
 	
 	public static void navigate(String urlKey) 
 	{
-		driver.get(p.getProperty(urlKey));
+		//driver.get(p.getProperty(urlKey));
+		driver.navigate().to(p.getProperty(urlKey));
 	}
 
 }
--- Night9_30 PM Batch/src/com/browsers/TC_001.java
+++ Night9_30 PM Batch/src/com/browsers/TC_001.java
@@ -1,5 +1,7 @@
 package com.browsers;
 
+import org.openqa.selenium.By;
+
 public class TC_001 extends BaseTest
 {
 
@@ -7,10 +9,38 @@ public class TC_001 extends BaseTest
 	{
 		init();
 		
-		launch("edgebrowser");
+		launch("chromebrowser");
 		
-		navigate("googleurl");
-
+		navigate("hdfcurl");
+		
+		driver.manage().window().maximize();
+		
+		/*String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().refresh();*/
+		
+		driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
+		
+		driver.findElement(By.id("loginsubmit")).click();
+		
+		driver.close();
+		
+		//driver.quit();
+		
 	}
 
 	
